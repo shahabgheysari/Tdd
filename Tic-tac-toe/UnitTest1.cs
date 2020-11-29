d6fa00efdbc6a145e83c00be7a82f873ba5a8155
Shahab Gheysari <ShahabGheysari@gmail.com>
2020-11-29 12:05:13 +0330

    tested space coordinate

--- Tic-tac-toe/UnitTest1.cs
+++ Tic-tac-toe/UnitTest1.cs
@@ -6,8 +6,31 @@ namespace Tic_tac_toe
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestSpaceHasRowAndColumn()
         {
+            Space space11 = new Space(1,2);
+            
+            Assert.Equal(space11.Row(),1);
+            Assert.Equal(space11.Column(),2);
+        }
+    }
+
+    public class Space
+    {
+        private int row;
+        private int column;
+        public Space(int row,int column)
+        {
+        }
+
+        public int Row()
+        {
+            return 1;
+        }
+
+        public int Column()
+        {
+            return 2;
         }
     }
 }
